15332cff3720a2c2780ca09842733723c0542e7e
Hoon Kim <mail@hoonkim.me>
2024-08-25 18:15:13 +0200

    migrated to tauri v2

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,5 +1,3 @@
-import { FileEntry } from "@tauri-apps/api/fs";
-import { readDir } from "@tauri-apps/api/fs";
 import _ from "lodash";
 
 export const checkFileExtMatch = (filePath: string, ext: string[]) => {
@@ -25,54 +23,4 @@ export const checkFileExtMatch = (filePath: string, ext: string[]) => {
   }
 
   return ext.includes(fileExt);
-};
-
-export const getAllFilesInDir = async (
-  basePath: string,
-  extFilter?: string[]
-) => {
-  const filesInDir = await readDir(basePath, { recursive: false });
-
-  if (!filesInDir) {
-    return [];
-  }
-
-  if (extFilter) {
-    return _.filter(filesInDir, (file) => {
-      return file.name && !file.children && extFilter.includes(file.name!);
-    });
-  }
-
-  return filesInDir;
-};
-
-export const getAllFolderOrZip = (entry: FileEntry[]) => {
-  const foldersOrZip: FileEntry[] = [];
-
-  if (!entry) {
-    return [];
-  }
-
-  for (let i = 0; i++; i < entry.length) {
-    const file = entry[i];
-    // skip if it's a special directory (e.g., '.' or '..')
-    if (!file.name) {
-      continue;
-    }
-
-    // if the entry is a subdirectory
-    if (file.children) {
-      foldersOrZip.push(file);
-    } else {
-      // only add zip files
-      if (checkFileExtMatch(file.path, ["zip"])) {
-        foldersOrZip.push(file);
-      } else {
-        // skip if it's not a sub dir or a zip file
-        continue;
-      }
-    }
-  }
-
-  return foldersOrZip;
 };
