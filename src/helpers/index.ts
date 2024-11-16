4f101103e7a9fd2fb3403f30f13aab4808a9bdc0
Hoon Kim <mail@hoonkim.me>
2024-11-16 01:09:44 +0100

    add sidecar config and bin for mac arm

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -5,6 +5,25 @@ import JSZip from "jszip";
 import { DocumentItem } from "../types";
 import { invoke } from "@tauri-apps/api/core";
 import { PDFDocument, PDFImage } from "pdf-lib";
+import { Command } from "@tauri-apps/plugin-shell";
+
+export const convertToPdfSidecar = async (fileName: string, inputPath: string, outputPath: string) => {
+  try {
+    const command = Command.sidecar("sidecar/python-script", [
+      "--name",
+      fileName,
+      "--input",
+      inputPath,
+      "--output",
+      outputPath,
+    ]);
+
+    const output = await command.execute();
+    console.log(output)
+  } catch (e) {
+    console.error(e);
+  }
+};
 
 export const getFileExt = (filePath: string) => {
   const ext = filePath.toLowerCase().split(".").pop();
@@ -62,18 +81,12 @@ const webpToPngRust = async (webpData: Uint8Array) => {
 
   const convertedData = new Uint8Array(convertedImg as Uint8Array);
 
-  console.log(
-    `Converted WebP image to PNG with the size of ${convertedData.length} bytes using Rust`
-  );
-  // trust issues
   return convertedData;
 };
 
 const embedImgToPdf = async (imgPath: string, pdf: PDFDocument) => {
   const imgBin = await readFile(imgPath);
   const imgExt = getFileExt(imgPath);
-
-  console.log(`Processing page ${imgPath}`);
 
   let imageToAdd: PDFImage;
 
@@ -110,8 +123,6 @@ const createPdfFromImages = async (imgPagePaths: string[]) => {
   // for example, if the pdf doesn't already exist, create a new one and write first chunk, next loop will load the written file and add the next chunk
   let pdfDoc = await PDFDocument.create();
 
-  console.log("Creating a new PDF document");
-
   for (const imgPath of imgPagePaths) {
     pdfDoc = await embedImgToPdf(imgPath, pdfDoc);
   }
@@ -128,10 +139,6 @@ export const createPdfFromCollection = async (
       `Skipping ${doc.collectionName} as zip support is not added yet`
     );
   } else {
-    console.log(
-      `Converting collection ${doc.collectionName} in ${doc.basePath}`
-    );
-
     // note: this can get really big
     const pdfBin = await createPdfFromImages(
       _.map(doc.content, (i) => {
@@ -142,15 +149,9 @@ export const createPdfFromCollection = async (
     const docName = doc.collectionName + ".pdf";
     const savePath = await join(outputPath, docName);
 
-    console.log(
-      `Finished binding ${docName}. The document size is ${pdfBin.length} bytes`
-    );
-
     //await writeBinaryFile(savePath, pdfBin); // note: the app runs out of memory here if the file size is large
 
     await writeBinaryInChunks(savePath, pdfBin);
-
-    console.log(`Saved new PDF ${doc.collectionName} to ${savePath}`);
   }
 };
 
@@ -160,10 +161,6 @@ export const writeBinaryInChunks = async (
   chunkSize: number = 1024 * 1024 * 8 // 8 MB
 ) => {
   let offset = 0;
-
-  console.log(
-    `Saving the data with the size of ${data.length} into chunks of ${chunkSize}`
-  );
 
   while (offset < data.length) {
     // Calculate the end of the current chunk
@@ -178,6 +175,5 @@ export const writeBinaryInChunks = async (
 
     // Move the offset forward by the size of the chunk
     offset = end;
-    console.log(`Wrote chunk from ${offset} to ${end} (${chunk.length} bytes)`);
   }
 };
