e90e6428d48cd0198a8f10966886b5e42d402666
Hoon Kim <mail@hoonkim.me>
2024-11-16 16:18:16 +0100

    add comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,210 @@
-import FileUpload from "./components/FileUpload";
-import CollectionList from "./components/CollectionList";
 import { useState, useCallback } from "react";
-import { DocumentItem } from "./types";
-import { CButton, CCol, CListGroup, CListGroupItem, CRow } from "@coreui/react";
-import { downloadDir } from "@tauri-apps/api/path";
-import { open } from "@tauri-apps/plugin-dialog";
-import * as helpers from "./helpers";
-import _ from "lodash";
-
-import "@coreui/coreui/dist/css/coreui.min.css";
+import { DocumentItem, FileItem } from "./types"; // Import custom types for document and file items
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCol,
+  CListGroup,
+  CListGroupItem,
+  CRow,
+} from "@coreui/react"; // Import CoreUI components
+import { downloadDir } from "@tauri-apps/api/path"; // Function to get the default download directory
+import { open } from "@tauri-apps/plugin-dialog"; // Dialog plugin for opening file/folder selectors
+import * as helpers from "./helpers"; // Custom helper functions
+import { basename, join } from "@tauri-apps/api/path"; // Path manipulation functions
+import _ from "lodash"; // Utility library
+import { readDir } from "@tauri-apps/plugin-fs"; // Function to read directory contents
+import { appConfig } from "./config"; // Application configuration
+import "@coreui/coreui/dist/css/coreui.min.css"; // Import CoreUI CSS
 
 function App() {
-  // async function greet() {
-  //   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  //   setGreetMsg(await invoke("greet", { name }));
-  // }
+  const [files, setFiles] = useState<DocumentItem[]>([]); // State to hold the list of processed files
+  const [isLoading, setIsLoading] = useState(false); // State to indicate if the app is currently loading
 
-  const [files, setFiles] = useState<DocumentItem[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
+  /**
+   * Processes a directory by reading its contents and categorizing them into image files, zip files, and folders.
+   * @param path - The path of the directory to process.
+   * @returns An array of processed document items.
+   */
+  const processDir = async (path: string) => {
+    let processedFiles: DocumentItem[] = [];
 
-  const onClickExport = useCallback(() => {
+    const files = await readDir(path); // Read the contents of the directory
+
+    if (!files) {
+      return [];
+    }
+
+    // Filter image files based on supported formats
+    const imageFiles = files.filter((file) => {
+      return (
+        file.isFile &&
+        helpers.checkFileExtMatch(file.name, appConfig.supportedImgFormat)
+      );
+    });
+
+    // Filter zip files
+    const zipFiles = files.filter((file) => {
+      return file.isFile && helpers.checkFileExtMatch(file.name, ["zip"]);
+    });
+
+    // Filter folders (directories)
+    const folders = files.filter((file) => {
+      return !!file.isDirectory;
+    });
+
+    if (!imageFiles.length && !zipFiles.length && !folders.length) {
+      throw new Error(`${path} is empty`);
+    }
+
+    console.log("Found the following files in " + path, {
+      imageFiles,
+      zipFiles,
+      folders,
+    });
+
+    // Handling image collections in the base directory
+    if (imageFiles.length > 0) {
+      const collectionName = await basename(path); // Get the name of the directory
+
+      const collectionContent = await Promise.all(
+        _.map(imageFiles, async (i) => {
+          return {
+            name: i.name,
+            path: await join(path, i.name),
+          } as FileItem;
+        })
+      );
+
+      const rootCollection: DocumentItem = {
+        collectionName: collectionName,
+        basePath: path,
+        content: _.sortBy(collectionContent, ["name"]), // Sort files by name
+        isArchive: false,
+      };
+
+      processedFiles.push(rootCollection);
+    }
+
+    // Handling folders of image collections
+    if (folders.length > 0) {
+      const subDocs = _.map(folders, async (folder) => {
+        return await processDir(await join(path, folder.name)); // Recursively process each folder
+      });
+
+      const processedSubDirs = (await Promise.all(subDocs)).flat();
+      processedFiles.push(...processedSubDirs);
+    }
+
+    // Handling zip files of image collections
+    if (zipFiles.length > 0) {
+      const zipCollections = await Promise.all(
+        _.map(zipFiles, async (i) => {
+          return {
+            collectionName: i.name,
+            basePath: await join(path, i.name),
+            content: [],
+            isArchive: true,
+          } as DocumentItem;
+        })
+      );
+
+      processedFiles.push(...zipCollections);
+    }
+
+    console.log("Processed collections ", processedFiles);
+
+    return processedFiles;
+  };
+
+  /**
+   * Handles the drop event when files or folders are dragged and dropped onto the drop zone.
+   * @param event - The drag event.
+   */
+  const handleDrop = useCallback((event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    const items = event.dataTransfer.items;
+
+    if (items) {
+      const filePromises: Promise<FileItem[]>[] = [];
+      for (let i = 0; i < items.length; i++) {
+        const item = items[i].webkitGetAsEntry();
+        if (item) {
+          filePromises.push(processEntry(item as FileSystemDirectoryEntry)); // Process each entry
+        }
+      }
+
+      // Promise.all(filePromises).then((fileItems) => {
+      //   setFiles(fileItems.flat());
+      // });
+    }
+  }, []);
+
+  /**
+   * Recursively processes a directory or file entry.
+   * @param entry - The file system entry to process (can be a file or a directory).
+   * @returns An array of processed file items.
+   */
+  const processEntry = async (
+    entry: FileSystemDirectoryEntry
+  ): Promise<FileItem[]> => {
+    if (entry.isFile) {
+      return [{ path: entry.fullPath, name: entry.name || "" }]; // Return the file item for a file entry
+    } else if (entry.isDirectory) {
+      const dirReader = entry.createReader();
+      const entries = await new Promise<any[]>((resolve) => {
+        dirReader.readEntries(resolve); // Read the contents of the directory
+      });
+
+      const promises = entries.map((e) => processEntry(e)); // Process each entry in the directory
+      const results = await Promise.all(promises);
+      return results.flat();
+    }
+    return [];
+  };
+
+  /**
+   * Handles the drag over event to allow dropping files or folders.
+   * @param event - The drag event.
+   */
+  const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+  };
+
+  /**
+   * Opens a folder selection dialog and processes the selected directories.
+   */
+  const onClickSelectFiles = async () => {
+    setIsLoading(true); // Show loading effect
+
+    const selected = await open({
+      multiple: true,
+      directory: true,
+      recursive: false,
+    });
+
+    if (!selected) {
+      console.error("User did not select a folder");
+      setIsLoading(false);
+      return;
+    }
+
+    if (Array.isArray(selected)) {
+      const filePromises = selected.map(async (path) => {
+        return await processDir(path); // Process each selected directory
+      });
+
+      const fileItems = (await Promise.all(filePromises)).flat();
+      setFiles(fileItems); // Update the files state with processed items
+      setIsLoading(false);
+    }
+  };
+
+  /**
+   * Converts the selected document collections into PDFs.
+   */
+  const onClickConvert = useCallback(() => {
     setIsLoading(true);
 
     const exportPdf = async () => {
@@ -38,11 +223,9 @@ function App() {
       console.log("Got the export path: ", saveLoc);
 
       if (files.length > 0) {
-        console.log(`There are ${files.length} collections`);
-
         for (let i = 0; i < files.length; i++) {
           const file = files[i];
-          await helpers.createPdfFromCollection(file, saveLoc.toString());
+          await helpers.createPdfFromCollection(file, saveLoc.toString()); // Convert each collection to a PDF
         }
       }
     };
@@ -52,7 +235,7 @@ function App() {
         console.error(err);
       })
       .finally(() => {
-        setIsLoading(false);
+        setIsLoading(false); // Hide loading effect after processing is complete
       });
   }, [files]);
 
@@ -62,26 +245,51 @@ function App() {
       style={{
         paddingTop: "20px",
         paddingBottom: "20px",
-        position: "relative"
+        position: "relative",
       }}
     >
       <CRow>
         <CCol>
-          <div style={{
-            height: "100%"
-          }}>
-            <FileUpload onFileSelect={(i) => setFiles(i)} />
-            {/* <CollectionList collections={files} /> */}
-            <CButton
-              color="primary"
-              disabled={files.length < 1 || isLoading}
-              onClick={onClickExport}
-            >
-              Convert
-            </CButton>
-          </div>
+          <CCard
+            style={{
+              height: "100%",
+              width: "100%",
+            }}
+          >
+            <CCardBody>
+              {/* Drag and drop area */}
+              <div
+                onDrop={handleDrop}
+                onDragOver={handleDragOver}
+                style={{
+                  border: "2px dashed #ccc",
+                  padding: "20px",
+                  marginBottom: "20px",
+                }}
+              >
+                Drag and drop
+              </div>
+              {/* Button to select files or folders */}
+              <CButton
+                color="primary"
+                onClick={onClickSelectFiles}
+                disabled={isLoading}
+              >
+                {isLoading ? "Loading files..." : "Select Files or Folders"}
+              </CButton>
+              {/* Button to convert selected collections to PDFs */}
+              <CButton
+                color="primary"
+                disabled={files.length < 1 || isLoading}
+                onClick={onClickConvert}
+              >
+                Convert
+              </CButton>
+            </CCardBody>
+          </CCard>
         </CCol>
         <CCol>
+          {/* Display processed files and collections */}
           <div
             style={{
               height: "90vh",
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,33 +1,49 @@
-import { readFile, writeFile } from "@tauri-apps/plugin-fs";
-import { join } from "@tauri-apps/api/path";
-import _ from "lodash";
-import JSZip from "jszip";
-import { DocumentItem } from "../types";
-import { invoke } from "@tauri-apps/api/core";
-import { PDFDocument, PDFImage } from "pdf-lib";
-import { Command } from "@tauri-apps/plugin-shell";
+import { readFile, writeFile } from "@tauri-apps/plugin-fs"; // Import functions to read and write files from Tauri's file system plugin
+import { join } from "@tauri-apps/api/path"; // Import function to join paths safely
+import _ from "lodash"; // Import lodash for utility functions
+import JSZip from "jszip"; // Import JSZip library to handle zip files
+import { DocumentItem } from "../types"; // Import custom type definitions for document items
+import { invoke } from "@tauri-apps/api/core"; // Import function to invoke Rust commands from Tauri
+import { PDFDocument, PDFImage } from "pdf-lib"; // Import classes to create and manipulate PDF documents
+import { Command } from "@tauri-apps/plugin-shell"; // Import command execution utility
 
-export const convertToPdfSidecar = async (fileName: string, inputPath: string, outputPath: string) => {
-  try {
-    const command = Command.sidecar("bin/python-script", [
-      "--name",
-      fileName,
-      "--input",
-      inputPath,
-      "--output",
-      outputPath,
-    ]);
+/**
+ * Converts a file to PDF using a sidecar Python script.
+ * @param fileName - The name of the file to convert.
+ * @param inputPath - The path to the input file.
+ * @param outputPath - The path where the output PDF should be saved.
+ */
+export const convertToPdfSidecar = async (
+  fileName: string,
+  inputPath: string,
+  outputPath: string
+) => {
+  // Create a command instance for the sidecar Python script with arguments
+  const command = Command.sidecar("bin/python-script", [
+    "--name",
+    fileName,
+    "--input",
+    inputPath,
+    "--output",
+    outputPath,
+  ]);
 
-    const output = await command.execute();
-    console.log(output)
-  } catch (e) {
-    console.error(e);
-  }
+  // Execute the command and log the output
+  const output = await command.execute();
+  console.log(output);
 };
 
-export const getFileExt = (filePath: string) => {
+/**
+ * Extracts the file extension from a given file path.
+ * @param filePath - The path of the file.
+ * @returns The file extension in lowercase.
+ * @throws Will throw an error if the file does not have an extension.
+ */
+const getFileExt = (filePath: string) => {
+  // Split the file path by dot and get the last part as the extension
   const ext = filePath.toLowerCase().split(".").pop();
 
+  // Throw an error if no extension is found
   if (!ext) {
     throw new Error(`${filePath} does not have an extension`);
   }
@@ -35,61 +51,102 @@ export const getFileExt = (filePath: string) => {
   return ext;
 };
 
+/**
+ * Checks if a file's extension matches any of the provided extensions.
+ * @param filePath - The path of the file to check.
+ * @param ext - An array of valid extensions.
+ * @returns True if the file's extension is in the list, false otherwise.
+ */
 export const checkFileExtMatch = (filePath: string, ext: string[]) => {
   return ext.includes(getFileExt(filePath));
 };
 
+/**
+ * Reads a ZIP file and returns its contents as a JSZip object.
+ * @param zipPath - The path to the ZIP file.
+ * @returns A JSZip object containing the ZIP file's contents.
+ */
 export const readZipFile = async (zipPath: string) => {
-  const zipData = await readFile(zipPath); // todo: needs to be optimized
+  // Read the binary data of the ZIP file
+  const zipData = await readFile(zipPath);
 
+  // Load the ZIP data into a JSZip object
   return await JSZip.loadAsync(zipData);
 };
 
+/**
+ * Converts WebP image data to PNG format using JavaScript.
+ * @param webpData - The binary data of the WebP image.
+ * @returns The binary data of the converted PNG image.
+ */
 export const webpToPng = async (webpData: Uint8Array) => {
-  // create an image from the WebP data
+  // Create an ImageBitmap from the WebP data
   const img = await createImageBitmap(new Blob([webpData]));
 
-  // create a canvas element to draw the image
+  // Create a canvas element to draw the image
   const canvas = document.createElement("canvas");
   canvas.width = img.width;
   canvas.height = img.height;
+
   const ctx = canvas.getContext("2d");
 
+  // Throw an error if the 2D context is not available
   if (!ctx) {
     throw new Error("Cannot find context 2d in the canvas!");
   }
 
+  // Draw the image on the canvas
   ctx.drawImage(img, 0, 0);
 
-  // convert the canvas content to a PNG data URL
+  // Convert the canvas content to a PNG data URL
   const pngUri = canvas.toDataURL("image/png");
 
+  // Fetch the PNG data URL and convert it to a blob
   const pngResponse = await fetch(pngUri);
   const pngBlob = await pngResponse.blob();
-  const pngArrayBuffer = await pngBlob.arrayBuffer();
 
+  // Convert the blob to an array buffer and return as Uint8Array
+  const pngArrayBuffer = await pngBlob.arrayBuffer();
   return new Uint8Array(pngArrayBuffer);
 };
 
+/**
+ * Converts WebP image data to PNG format using a Rust function.
+ * @param webpData - The binary data of the WebP image.
+ * @returns The binary data of the converted PNG image.
+ */
 const webpToPngRust = async (webpData: Uint8Array) => {
+  // Invoke the Rust function to convert WebP to PNG
   const convertedImg = await invoke("convert_webp_to_png", {
     webpData: Array.from(webpData),
   });
+
+  // Throw an error if the conversion failed
   if (typeof convertedImg === "string") {
     throw new Error(convertedImg);
   }
 
+  // Convert the result to Uint8Array and return
   const convertedData = new Uint8Array(convertedImg as Uint8Array);
-
   return convertedData;
 };
 
+/**
+ * Embeds an image into a PDF document.
+ * @param imgPath - The path to the image file to embed.
+ * @param pdf - The PDFDocument object to which the image will be added.
+ * @returns The updated PDFDocument with the embedded image.
+ */
 const embedImgToPdf = async (imgPath: string, pdf: PDFDocument) => {
+  // Read the binary data of the image
   const imgBin = await readFile(imgPath);
+
+  // Get the file extension of the image
   const imgExt = getFileExt(imgPath);
 
   let imageToAdd: PDFImage;
 
+  // Embed the image based on its format
   switch (imgExt) {
     case "png":
       imageToAdd = await pdf.embedPng(imgBin);
@@ -105,9 +162,11 @@ const embedImgToPdf = async (imgPath: string, pdf: PDFDocument) => {
     default:
       throw new Error(`File extension ${imgExt} is not supported`);
   }
-  // note: this part will progressively get bigger as the number of processed images increase
-  // we need to find a way to process images into chunks, save it before processing the next chunk
+
+  // Add a page to the PDF with the same dimensions as the image
   const page = pdf.addPage([imageToAdd.width, imageToAdd.height]);
+
+  // Draw the image on the page
   page.drawImage(imageToAdd, {
     x: 0,
     y: 0,
@@ -118,44 +177,59 @@ const embedImgToPdf = async (imgPath: string, pdf: PDFDocument) => {
   return pdf;
 };
 
+/**
+ * Creates a PDF document from an array of image paths.
+ * @param imgPagePaths - An array of file paths to images to include in the PDF.
+ * @returns The binary data of the created PDF document.
+ */
 const createPdfFromImages = async (imgPagePaths: string[]) => {
-  // todo: this function does not scale well. Maybe add pages in chunks?
-  // for example, if the pdf doesn't already exist, create a new one and write first chunk, next loop will load the written file and add the next chunk
+  // Create a new PDF document
   let pdfDoc = await PDFDocument.create();
 
+  // Embed each image into the PDF document
   for (const imgPath of imgPagePaths) {
     pdfDoc = await embedImgToPdf(imgPath, pdfDoc);
   }
+
+  // Save the PDF document and return its binary data
   return await pdfDoc.save({ useObjectStreams: true });
 };
 
-// todo: refactor this to use the python script sidecar
+/**
+ * Creates a PDF document from a collection of images or archived content.
+ * @param doc - A DocumentItem object representing the collection to convert.
+ * @param outputPath - The path where the output PDF should be saved.
+ */
 export const createPdfFromCollection = async (
   doc: DocumentItem,
   outputPath: string
 ) => {
   if (doc.isArchive) {
-    // todo: implement convert zip content to pdf
+    // Skip archives as zip support is not implemented yet
     console.log(
       `Skipping ${doc.collectionName} as zip support is not added yet`
     );
   } else {
-    // note: this can get really big
+    // Create a PDF document from the images in the collection
     const pdfBin = await createPdfFromImages(
-      _.map(doc.content, (i) => {
-        return i.path;
-      })
+      _.map(doc.content, (i) => i.path)
     );
 
+    // Construct the output file path
     const docName = doc.collectionName + ".pdf";
     const savePath = await join(outputPath, docName);
 
-    //await writeBinaryFile(savePath, pdfBin); // note: the app runs out of memory here if the file size is large
-
+    // Write the PDF binary data to the file in chunks to avoid memory issues
     await writeBinaryInChunks(savePath, pdfBin);
   }
 };
 
+/**
+ * Writes binary data to a file in chunks.
+ * @param destinationPath - The path where the file should be saved.
+ * @param data - The binary data to write.
+ * @param chunkSize - The size of each chunk (default: 8 MB).
+ */
 export const writeBinaryInChunks = async (
   destinationPath: string,
   data: Uint8Array,
@@ -163,6 +237,7 @@ export const writeBinaryInChunks = async (
 ) => {
   let offset = 0;
 
+  // Loop through the data in chunks and write each chunk to the file
   while (offset < data.length) {
     // Calculate the end of the current chunk
     const end = Math.min(offset + chunkSize, data.length);
@@ -170,7 +245,6 @@ export const writeBinaryInChunks = async (
     // Extract the current chunk from the Uint8Array
     const chunk = data.slice(offset, end);
 
-    // todo: there is an issue with the chunk saving, where not all the bytes will be saved correctly
     // Write the chunk to the destination file
     await writeFile(destinationPath, chunk, { append: true });
 
